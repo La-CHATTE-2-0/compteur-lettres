285e373d387c825775bf64ccf7e1abdf20cf9d7a
Alan LECART <alan.lecart@outlook.com>
2022-01-14 20:51:57 +0100

    add common custom char

--- src/App/App.js
+++ src/App/App.js
@@ -13,6 +13,13 @@ function App() {
     setValue(event.target.value);
   };
 
+  const handleCustomInput = (event) => {
+    console.log(event.target.textContent)
+    let inputElement = document.getElementById("input")
+    inputElement.innerHTML += event.target.textContent
+    setValue(inputElement.innerHTML);
+  };
+
   function canExport() {
     return value.length > 1 && ![...value].every(char => { return excludedChars.includes(char) })
   }
@@ -30,10 +37,9 @@ function App() {
 
   return (
     <div className="App">
-      <label htmlFor="input" className="form-label">Lettres</label>
-      <textarea className="form-control mb-2" id="input" value={value} onChange={handleChange}></textarea>
+      <textarea className="form-control mt-2 mb-2" id="input" value={value} onChange={handleChange}></textarea>
 
-      <div className="form-check form-switch">
+      <div className="form-check form-switch mb-2">
         <input
           className='form-check-input'
           type="checkbox"
@@ -44,7 +50,42 @@ function App() {
         <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Insignes identiques</label>
       </div>
 
-      <button id="export" type="button" className="btn btn-dark" onClick={handleExport}>Export en XLS</button>
+      <div className='btn-group mb-2'>
+        {
+          "¹²³⁴⁵⁶⁷⁸⁹⁰".split("").map(element => {
+            return (
+              <button
+                onClick={handleCustomInput}
+                type="button"
+                className="btn btn-outline-secondary"
+                key={element}
+              >
+                {element}
+              </button>
+            )
+          })
+        }
+      </div>
+      <div className='btn-group mb-2'>
+        {
+          ["β", "ε", "ϕ", "𝛾"].map(element => {
+            return (
+              <button
+                onClick={handleCustomInput}
+                type="button"
+                className="btn btn-outline-secondary"
+                key={element}
+              >
+                {element}
+              </button>
+            )
+          })
+        }
+      </div>
+
+      <div>
+        <button id="export" type="button" className="btn btn-dark" onClick={handleExport}>Export en XLS</button>
+      </div>
       <OccurencesTable text={value} boolMemeInsigne={memeInsigne} />
     </div>
   );
